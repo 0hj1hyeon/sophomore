47ae6b83636a0855d2528c717a09433a0936124f
0hj1hyeon <ojh010911@gmail.com>
2023-11-02 21:08:07 +0900

    23.11.02

--- 데이터구조2/11장_연습문제/11장연습문제7번/11장연습문제7번/11장연습문제7번.c
+++ 데이터구조2/11장_연습문제/11장연습문제7번/11장연습문제7번/11장연습문제7번.c
@@ -4,62 +4,115 @@
 
 #define TRUE 1
 #define FALSE 0
-#define MAX_VERTICES	100	
-#define INF	1000000	/* 무한대 (연결이 없는 경우) */
+#define MAX_VERTICES 100
+#define INF 1000000 /* 무한대 (연결이 없는 경우) */
+
+typedef struct GraphNode {
+    int vertex;
+    int weight;
+    struct GraphNode* next;
+} GraphNode;
 
 typedef struct GraphType {
-	int n;	// 정점의 개수
-	int weight[MAX_VERTICES][MAX_VERTICES];
+    int n;                 // 정점의 개수
+    GraphNode* adj_list[MAX_VERTICES];
 } GraphType;
 
-int distance[MAX_VERTICES];/* 시작정점으로부터의 최단경로 거리 */
-int found[MAX_VERTICES];		/* 방문한 정점 표시 */
+int distance[MAX_VERTICES]; /* 시작정점으로부터의 최단경로 거리 */
+int found[MAX_VERTICES];    /* 방문한 정점 표시 */
+int previous[MAX_VERTICES]; /* 이전 정점 저장 */
 
-int choose(int distance[], int n, int found[])
-{
-	int i, min, minpos;
-	min = INT_MAX;
-	minpos = -1;
-	for (i = 0; i < n; i++)
-		if (distance[i] < min && !found[i]) {
-			min = distance[i];
-			minpos = i;
-		}
-	return minpos;
-}
-//
-void shortest_path(GraphType* g, int start)
-{
-	int i, u, w;
-	for (i = 0; i < g->n; i++) /* 초기화 */
-	{
-		distance[i] = g->weight[start][i];
-		found[i] = FALSE;
-	}
-	found[start] = TRUE;    /* 시작 정점 방문 표시 */
-	distance[start] = 0;
-	for (i = 0; i < g->n - 1; i++) {
-		print_status(g);
-		u = choose(distance, g->n, found);
-		found[u] = TRUE;
-		for (w = 0; w < g->n; w++)
-			if (!found[w])
-				if (distance[u] + g->weight[u][w] < distance[w])
-					distance[w] = distance[u] + g->weight[u][w];
-	}
+int choose(int distance[], int n, int found[]) {
+    int i, min, minpos;
+    min = INT_MAX;
+    minpos = -1;
+    for (i = 0; i < n; i++)
+        if (distance[i] < min && !found[i]) {
+            min = distance[i];
+            minpos = i;
+        }
+    return minpos;
 }
 
-int main(void)
-{
-	GraphType g = { 7,
-	{{ 0,  7,  INF, INF,   3,  10, INF },
-	{ 7,  0,    4,  10,   2,   6, INF },
-	{ INF,  4,    0,   2, INF, INF, INF },
-	{ INF, 10,    2,   0,  11,   9,   4 },
-	{ 3,  2,  INF,  11,   0, INF,   5 },
-	{ 10,  6,  INF,   9, INF,   0, INF },
-	{ INF, INF, INF,   4,   5, INF,   0 } }
-	};
-	shortest_path(&g, 0);
-	return 0;
+void add_edge(GraphType* g, int start, int end, int weight) {
+    GraphNode* new_node = (GraphNode*)malloc(sizeof(GraphNode));
+    new_node->vertex = end;
+    new_node->weight = weight;
+    new_node->next = g->adj_list[start];
+    g->adj_list[start] = new_node;
+}
+
+void shortest_path(GraphType* g, int start) {
+    int i, u, w;
+    for (i = 0; i < g->n; i++) {
+        distance[i] = INF;
+        found[i] = FALSE;
+        previous[i] = -1; // Initialize previous array
+    }
+    distance[start] = 0;
+
+    for (i = 0; i < g->n - 1; i++) {
+        u = choose(distance, g->n, found);
+        found[u] = TRUE;
+        GraphNode* current = g->adj_list[u];
+        while (current != NULL) {
+            w = current->vertex;
+            if (!found[w] && distance[u] + current->weight < distance[w]) {
+                distance[w] = distance[u] + current->weight;
+                previous[w] = u; // Update previous vertex
+            }
+            current = current->next;
+        }
+         
+        printf("Distance array after step %d:\n", i + 1); // 데구 9번
+        for (int j = 0; j < g->n; j++) {
+            printf("distance[%d]: %d\n", j, distance[j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_path(int start, int end) {
+    if (start == end) {
+        printf("%d ", start);
+        return;
+    }
+    print_path(start, previous[end]);
+    printf("%d ", end);
+}
+
+void print_shortest_paths(GraphType* g, int start) { // 데구 8버
+    printf("Shortest paths from vertex %d:\n", start);
+    for (int i = 0; i < g->n; i++) {
+        if (i != start) {
+            printf("To vertex %d: ", i);
+            print_path(start, i);
+            printf(" (Distance: %d)\n", distance[i]);
+        }
+    }
+}
+
+int main(void) {
+    GraphType g;
+    g.n = 6;
+
+    for (int i = 0; i < g.n; i++) {
+        g.adj_list[i] = NULL;
+    }
+
+    add_edge(&g, 0, 1, 50);
+    add_edge(&g, 0, 2, 45);
+    add_edge(&g, 0, 3, 10);
+    add_edge(&g, 1, 2, 10);
+    add_edge(&g, 1, 3, 15);
+    add_edge(&g, 2, 4, 30);
+    add_edge(&g, 3, 0, 20);
+    add_edge(&g, 3, 4, 15);
+    add_edge(&g, 4, 1, 20);
+    add_edge(&g, 4, 2, 35);
+    add_edge(&g, 5, 4, 3); // 정점 5 추가
+
+    shortest_path(&g, 0);
+    print_shortest_paths(&g, 0); // Print shortest paths
+    return 0;
 }
