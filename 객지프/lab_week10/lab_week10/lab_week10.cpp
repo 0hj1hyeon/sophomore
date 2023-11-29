aec40d3a1ce9bcd13add20cb39bb0f7996af2077
0hj1hyeon <ojh010911@gmail.com>
2023-11-29 21:40:01 +0900

    23.11.29

---
+++ 객지프/lab_week10/lab_week10/lab_week10.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+class Complex {
+    double re, im;
+
+public:
+    Complex(double r = 0.0, double i = 0.0) {
+        re = r; im = i;
+    }
+    void show() {
+        cout << re << " + " << im << " i " << endl;
+    }
+    /*Complex& operator+(const Complex& c1) {
+        Complex c;
+        c.re = this->re + c1.re;
+        c.im = this->im + c1.im;
+        return c;
+       
+    }
+  
+    Complex& operator+=(const Complex& c1) {
+
+       this->re += c1.re;
+       this->im += c1.im;
+       return *this;
+    }*/
+    friend Complex& operator+(Complex& c1, Complex& c2);
+    friend Complex& operator+=(Complex& c1, Complex& c2);
+
+};
+Complex& operator+(Complex& c1, Complex& c2) {
+    Complex temp;
+    temp.re = c1.re + c2.re;
+    temp.im = c1.im + c2.im;
+    return temp;
+}
+
+Complex& operator+=(Complex& c1, Complex& c2) {
+    c1.re = c1.re + c2.re;
+    c1.im = c1.im + c2.im;
+    return c1;
+}
+
+int main() {
+    Complex c1(1, 2), c2(3, 4), c3;
+    c3 = c1 + c2;
+    cout << "\nAfter+: " << endl;
+    c3.show();
+
+    c3 += c1;
+    cout << "\nAfter+: " << endl;
+    c3.show();
+
+    return 0;
+  
+
+}
+
---
+++ 객지프/lab_week10_2/lab_week10_2/lab_week10_2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+class Complex {
+    double re, im;
+
+public:
+    Complex(double r = 0.0, double i = 0.0) {
+        re = r; im = i;
+    }
+    void show() {
+        cout << re << " + " << im << " i " << endl;
+    }
+    /*Complex& operator++() {
+        this->re++;
+        this->im++;
+        return *this;
+    }
+    Complex& operator++(int i) {
+        Complex temp = *this;
+        this->re++;
+        this->im++;
+        return temp;
+    }*/
+    friend Complex& operator++(Complex& c);
+    friend Complex& operator++(Complex& c, int i);
+    friend ostream& operator<<(ostream& os, const Complex& c);
+     
+
+};
+ostream& operator<<(ostream& os, const Complex& c) {
+    os << c.re << "+" << c.im << "i" << endl;
+    return os;
+}
+Complex& operator++(Complex& c) {
+    c.re++;
+    c.im++;
+    return c;
+}
+
+Complex& operator++(Complex& c, int i) {
+    Complex temp = c;
+    c.re++;
+    c.im++;
+    return temp;
+}
+int main() {
+    Complex c1(1, 2), c2;
+    cout << c1 << endl;
+    cout << c2 << endl;
+
+    c2 = ++c1;
+    cout << "\nAfter prefix++: " << endl;
+    cout << c1 << c2 << endl;
+    c2 = c1++;
+    cout << "\nAfter prefix++: " << endl;
+    cout << c1 << c2 << endl;
+    return 0;
+}
