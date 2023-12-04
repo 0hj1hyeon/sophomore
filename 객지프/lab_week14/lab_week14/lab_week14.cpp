513b536461f3e89ec4dec4ad864ea312b6a6d3c6
0hj1hyeon <ojh010911@gmail.com>
2023-12-05 03:47:05 +0900

    23.12.05

---
+++ 객지프/lab_week14/lab_week14/lab_week14.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    using namespace std;
+    ofstream os("dict.txt");
+    if (!os) {
+        cerr << "파일 오픈에 실패" << endl;
+        exit(1);
+
+    }
+    
+    ifstream is("test.txt");
+    if (!is) {
+        cerr << "파일 오픈에 실패" << endl;
+        exit(1);
+
+    }
+    vector<string> vc;
+    string a;
+    while (is>>a) {
+        vc.push_back(a);
+    
+    }
+    sort(vc.begin(), vc.end(), [](string s1, string s2) {return  s1 < s2; });
+
+    for (auto a : vc) {
+        cout << a << endl;
+        os << a << endl;
+    
+    
+    }
+    return 0;
+}
+
---
+++ 객지프/lab_week14_2/lab_week14_2/lab_week14_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool is_div4(int n) {
+    return (n % 4 == 0);
+}
+int main()
+{
+    vector<int>vc = {1,7,4,9,4,8,12,10,20};
+    int count = count_if(vc.begin(), vc.end(), is_div4);
+    cout << count << endl;
+    
+}
